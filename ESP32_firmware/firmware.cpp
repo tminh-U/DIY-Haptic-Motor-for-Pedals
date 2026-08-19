ac183f7dd803dde44fffb47d80e0dd1905cf5f6b
tminh <179489923+tminh-U@users.noreply.github.com>
2026-08-19 11:57:58 +0700

    Updating road effect formula

--- ESP32_firmware/firmware.cpp
+++ ESP32_firmware/firmware.cpp
@@ -5,12 +5,8 @@ const uint32_t SAMPLE_RATE = 5000;
 const float EPS = 1e-6;
 const float TWO_PI = 6.2831852f;
 
-// ================= CẤU HÌNH PHẦN CỨNG =================
-// Đèn LED tích hợp trên bo mạch ESP32 DevKit V1 (Onboard Blue LED)
-// Hầu hết các bản ESP32 DevKit V1 kết nối LED này vào chân GPIO 2
-const int LED_PIN = 2; 
 
-// Chân DAC (khi nào có dây điện thì cắm chân này sang amply)
+const int LED_PIN = 2; 
 const int DAC_PIN = 25; 
 
 
@@ -25,8 +21,6 @@ float SusR    = 0.0f;
 const float dt = 1.0f / SAMPLE_RATE;
 
 
-
-
 float cur_slip = 0.0f, cur_sus = 0.0f;
 float pre_absVal = 0.0f, pre_brakeVal = 0.0f, pre_slip = 0.0f, pre_sus = 0.0f;
 
@@ -38,18 +32,13 @@ void serial_read(void *pvParameters) {
                 swap(cur_absVal, pre_absVal);
                 swap(cur_brakeVal, pre_brakeVal);
                 swap(cur_slip, pre_slip);
-                swap(cur_sus, pre_sus);
+                swap(cur_susL, pre_susL);
+                swap(cur_susR, pre_susR);
 
 
-                int count = sscanf(packet.c_str(), "%f, %f, %f, %f, %f, %f", &cur_absVal, &cur_brakeVal, &slipL, &slipR, &SusL, &SusR);
+                int count = sscanf(packet.c_str(), "%f, %f, %f, %f, %f, %f", &cur_absVal, &cur_brakeVal, &slipL, &slipR, &cur_susL, &cur_susR);
                 digitalWrite(LED_PIN, (count == 6 ? HIGH : LOW));
-
-
-               
-                cur_slip = max(slipL, slipR);
-                cur_sus = max(abs(SusL), abs(SusR));
-
-                     
+                cur_slip = max(slipL, slipR);                     
         }
         vTaskDelay(pdMS_TO_TICKS(1));
     }
@@ -89,7 +78,7 @@ void IRAM_ATTR calc_effect() {
     t_sus += dt;
     if (t_sus > 1.0f) t_sus -= 1.0f;
 
-    float delta_sus = cur_sus - pre_sus;
+    float delta_sus = max(abs(cur_susL - pre_susL), abs(cur_susR - pre_susR));
     curg_delta_sus = EMAsmooth(delta_sus, curg_delta_sus);
     float road_effect = fminf(fabsf(curg_delta_sus) * (60.0f / 0.09f), 60.0f) * sinf(TWO_PI * 100.0f * t_sus);
                     
